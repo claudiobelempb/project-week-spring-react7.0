b0a2d3de83977f51db9a9224f90441a719f59cb6
claudiobelempb <claudio.c.lima@hotmail.com>
2022-03-11 22:59:07 -0300

    Save, useNavigate

--- frontend/src/app/http/index.ts
+++ frontend/src/app/http/index.ts
@@ -5,10 +5,10 @@ import { BASE_URL_API } from 'app/utils/constants';
 // const NEXT_PRODUCTION = `${process.env.NEXT_PRODUCTION}`;
 // console.log('NEXT_PUBLIC: ', NEXT_PUBLIC);
 // console.log('NEXT_PRODUCTION: ', NEXT_PRODUCTION);
-console.log(BASE_URL_API);
+console.log('BASE_URL', BASE_URL_API);
 
 const api = axios.create({
-  baseURL: `${process.env.BASE_URL_API}`
+  baseURL: `${BASE_URL_API}`
 });
 
 export { api };

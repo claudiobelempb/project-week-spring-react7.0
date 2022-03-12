1b6a7528d47e3c35d81894de214168b78a57ceda
claudiobelempb <claudio.c.lima@hotmail.com>
2022-03-11 21:30:09 -0300

    Show score

--- frontend/src/components/MovieScore/index.tsx
+++ frontend/src/components/MovieScore/index.tsx
@@ -7,13 +7,14 @@ type MovieScoreProps = {
   score: number;
   count: number;
 };
+
 const MovieScore: React.FC<MovieScoreProps> = ({ score, count }) => {
   return (
     <div className='dsmovie-score-container'>
       <p className='dsmovie-score-value'>
         {score > 0 ? score.toFixed(1) : '-'}
       </p>
-      <MovieStars />
+      <MovieStars score={score} />
       {/* <FaStar /> */}
       {/* <MovieStars /> */}
       <p className='dsmovie-score-count text-white fs-6'>{count} avaliações</p>
--- frontend/src/components/MovieStars/index.tsx
+++ frontend/src/components/MovieStars/index.tsx
@@ -4,14 +4,56 @@ import { FaStar, FaStarHalfAlt, FaRegStar } from 'react-icons/fa';
 // import { ReactComponent as StarEmpty } from 'assets/img/starEmpty.svg';
 import './styles.css';
 
-const MovieStars: React.FC = () => {
+type ScoreProps = {
+  score: number;
+};
+
+type FillProps = {
+  fill: number;
+};
+
+const Star: React.FC<FillProps> = ({ fill }: FillProps) => {
+  switch (fill) {
+    case 0:
+      return <FaRegStar />;
+    case 1:
+      return <FaStar />;
+
+    default:
+      return <FaStarHalfAlt />;
+  }
+};
+
+const MovieStars: React.FC<ScoreProps> = ({ score }) => {
+  // EX:
+  // getFills(3.5) => [1, 1, 1, 0.5, 0]
+  // getFills(4.1) => [1, 1, 1, 1, 0.5]
+  function getFills(score: number) {
+    const fills = [0, 0, 0, 0, 0];
+
+    const integerPart = Math.floor(score);
+
+    for (let i = 0; i < integerPart; i++) {
+      fills[i] = 1;
+    }
+
+    const diff = score - integerPart;
+    if (diff > 0) {
+      fills[integerPart] = 0.5;
+    }
+
+    return fills;
+  }
+
+  const fills = getFills(score);
+
   return (
     <div className='dsmovie-stars-container'>
-      <FaStar />
-      <FaStar />
-      <FaStar />
-      <FaStarHalfAlt />
-      <FaRegStar />
+      <Star fill={fills[0]} />
+      <Star fill={fills[1]} />
+      <Star fill={fills[2]} />
+      <Star fill={fills[3]} />
+      <Star fill={fills[4]} />
       {/* <StarFull />
       <StarFull />
       <StarFull />
